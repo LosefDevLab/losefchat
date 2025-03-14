c91ed87c2b2752a424b8e8fe666ee92e266dea21
Alonglosef <along-losef@outlook.com>
2025-03-14 20:45:34 +0800

    整理代码

---
+++ boot.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+
+
+
+
+// Mod : 程序, Des.: LC主程序类模组,包含模式选择、启动、模组加载基本重要功能
+class 程序
+{
+    static void Main()
+    {
+        Console.WriteLine("欢迎使用LosefChat v1.0.r2.b46\n输入1 开始聊天,输入2 服务器,输入3 EXIT");
+
+        int choose = int.Parse(Console.ReadLine());
+        if (choose == 1)
+        {
+            Console.Write("你要用ipv4协议，还是用ipv6协议？(输入4或者6,乱输我们就要把你请出去了哦awa):");
+            int 选择 = int.Parse(Console.ReadLine());
+
+            Console.Write("请输入服务器 IP 地址: ");
+            string 服务器IP = Console.ReadLine();
+
+            Console.Write("请输入服务器端口号: ");
+            int 服务器端口号 = int.Parse(Console.ReadLine());
+
+            Client 客户端 = new Client();
+
+            // 模组加载区域
+            // 在这里加载模组
+            // 下为简易示例,服务端模式相同操作
+            Client.mod is_a_mod = new Client.mod(客户端);//<<< 必须要在参数表(括号)中加入它对应运行模式的对应类的已声明实例
+            Thread modthread = new Thread(() =>
+            {
+                is_a_mod.Start();
+            });
+            modthread.Start();
+            // 必须要使用一个线程来加载模组
+            // 照这个例子，结合模组作者的文档在下面加载模组
+
+            // 模组加载区域结束
+
+            客户端.Connect(选择, 服务器IP, 服务器端口号);
+        }
+        if (choose == 2)
+        {
+            Console.Write("请输入服务器端口号: ");
+            int 端口 = int.Parse(Console.ReadLine());
+
+            Server 服务器 = new Server(端口);
+
+            // 模组加载区域
+            // 在这里加载模组
+            // 下为简易示例
+            Server.mod is_a_mod = new Server.mod(服务器);//<<< 必须要在参数表(括号)中加入它对应运行模式的对应类的已声明实例
+            Thread modthread = new Thread(() =>
+            {
+                is_a_mod.Start();
+            });// 必须要使用一个线程来加载模组
+            // 照这个例子，结合模组作者的文档在下面加载模组
+
+            // 模组加载区域结束
+
+            服务器.Start();
+        }
+        if (choose == 3)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ src/cilent/Client.cs
@@ -0,0 +1,215 @@
+using System;
+using System.Text;
+using System.Threading;
+using System.Net.Sockets;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+
+//Mod : Client, Des.: LC原版客户端核心类模组
+public partial class Client
+{
+    public TcpClient tcpClient;
+    public TcpClient tcpClient2;
+    public NetworkStream clientStream;
+    public string logFilePath = "logclient.txt"; // Log file path
+    public StreamWriter logFile;
+    public string usernamecpy = "";
+
+    public Client()
+    {
+        // 确保日志文件存在
+        if (!File.Exists(logFilePath))
+        {
+            using (File.Create(logFilePath)) { }
+        }
+        // 初始化 StreamWriter
+        logFile = new StreamWriter(logFilePath, true);
+    }
+
+    ~Client()
+    {
+        // 关闭 StreamWriter
+        logFile?.Close();
+    }
+
+    public void Log(string message)
+    {
+        logFile.WriteLine($"{DateTime.Now}: {message}");
+        logFile.Flush();
+    }
+
+    public void Connect(int ipvx, string serverIP, int serverPort)
+    {
+        try
+        {
+            tcpClient = new TcpClient();
+            tcpClient2 = new TcpClient(AddressFamily.InterNetworkV6);
+
+            if (ipvx == 4)
+            {
+                tcpClient.Connect(serverIP, serverPort);
+                clientStream = tcpClient.GetStream();
+            }
+            else if (ipvx == 6)
+            {
+                tcpClient2.Connect(serverIP, serverPort);
+                clientStream = tcpClient2.GetStream();
+            }
+            else
+            {
+                Console.WriteLine("我不说了，不要乱输入吗？qwq你不爱我了qwq");
+                Console.WriteLine("既然这样，那我就放炸弹!10秒倒计时!");
+                Thread.Sleep(1000);
+                Console.WriteLine("9秒后爆炸");
+                Thread.Sleep(1000);
+                Console.WriteLine("8秒后爆炸");
+                Thread.Sleep(1000);
+                Console.WriteLine("7秒后爆炸");
+                Thread.Sleep(1000);
+                Console.WriteLine("6秒后爆炸");
+                Thread.Sleep(1000);
+                Console.WriteLine("5秒后爆炸");
+                Thread.Sleep(1000);
+                Console.WriteLine("4秒后爆炸");
+                Thread.Sleep(1000);
+                Console.WriteLine("3秒后爆炸");
+                Thread.Sleep(1000);
+                Console.WriteLine("2秒后爆炸");
+                Thread.Sleep(1000);
+                Console.WriteLine("1秒后爆炸");
+                Thread.Sleep(1000);
+                Console.WriteLine("装逼我让你飞起来!");
+                Environment.Exit(-2147483648);
+                return;
+            }
+
+            // 用户输入用户名，如果没有输入则使用计算机名称
+            Console.Write("请输入用户名（按 Enter 使用计算机名）: ");
+            string username = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(username))
+                username = Environment.MachineName;
+
+            usernamecpy = username;
+
+            // 发送用户名到服务器
+            SendMessage(username);
+
+            // 用户输入密码
+            Console.Write("请输入密码: ");
+            string password = Console.ReadLine();
+
+            // 发送密码到服务器
+            SendMessage(password);
+
+            Thread receiveThread = new Thread(new ThreadStart(ReceiveMessage));
+            receiveThread.Start();
+
+            // 显示连接成功提示信息
+            Console.WriteLine("已连接到服务器。输入 'exit' 以关闭客户端。");
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+
+                if (message.ToLower() == "exit")
+                {
+                    SendMessage("我下线了啊拜拜");
+                    tcpClient.Close();
+                    break;
+                }
+
+                SendMessage(message);
+            }
+        }
+        catch (Exception ex)
+        {
+            Log($"连接服务器时发生异常: {ex.Message}");
+        }
+    }
+
+    public void ReceiveMessage()
+    {
+        byte[] message = new byte[32567];
+        int bytesRead;
+
+        List<string> messages = new List<string>();
+        bool connectionMessageShown = false;
+
+        while (true)
+        {
+            bytesRead = 0;
+
+            try
+            {
+                bytesRead = clientStream.Read(message, 0, 32567);
+            }
+            catch
+            {
+                break;
+            }
+
+            if (bytesRead == 0)
+                break;
+
+            string data = Encoding.UTF8.GetString(message, 0, bytesRead);
+            messages.Add($"\a{DateTime.Now} > {data}");
+            string logtmp = $"{DateTime.Now} > {data}";
+            Log(logtmp);
+            // 清除控制台并重新打印所有消息
+            Console.Clear();
+            foreach (var msg in messages)
+            {
+                Console.WriteLine(msg);
+            }
+
+            if (!connectionMessageShown)
+            {
+                Console.WriteLine($"已连接到服务器。输入 'exit' 以关闭客户端。");
+                Log($"我({usernamecpy})已连接到服务器。输入 'exit' 以关闭客户端。");
+                connectionMessageShown = true;
+            }
+        }
+    }
+
+    public void SendMessage(string message)
+    {
+        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+        clientStream.Write(messageBytes, 0, messageBytes.Length);
+        clientStream.Flush();
+    }
+
+    // Mod开发区域
+    // 以下空间供Mod的开发
+    // Mod开发规则:
+    // 一个mod只能使用一个Class,Class名称必须为mod名称
+    // Mod必须要有一个构造函数,构造函数必须要有一个Client/Server形参,并且在模组类内部创建一个和形参同等类型的对象
+    // 使这个内部对象和实参(形参)完全一样
+    // 必须为public类
+
+    // Mod : mod, Des.: 简易模组示例
+    public class mod
+    {
+        public Client clientcpy;
+        public mod(Client client) {
+            Thread a = new Thread(() => {
+                while(true) clientcpy = client;
+            });
+            a.Start();
+        }
+        public string Name {
+            get;
+            set;
+        }
+        public string Description {
+            get;
+            set;
+        }
+        public void Start()
+        {
+            // Console.WriteLine();
+        }
+    }
+}
